e5214a95d5493ba6f6c8f77b69540ba213facb10
Hemanth Krishna <hkpdev008@gmail.com>
2020-06-02 07:22:06 +0530

    Added bottom navigation bar

--- app/src/main/java/com/benrostudios/conciseo/ui/MainActivity.kt
+++ app/src/main/java/com/benrostudios/conciseo/ui/MainActivity.kt
@@ -2,12 +2,20 @@ package com.benrostudios.conciseo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.benrostudios.conciseo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment_main_activity)
+        bottom_navigation_view_main_activity.setupWithNavController(navController)
     }
 }
